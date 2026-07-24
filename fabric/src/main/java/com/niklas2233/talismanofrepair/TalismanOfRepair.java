3c35be8fa6d63399382334aed90ac39bb7224bf6
Niklas <niklas@example.com>
2026-07-25 01:20:39 +0200

    Scaffold Fabric mod project

---
+++ fabric/src/main/java/com/niklas2233/talismanofrepair/TalismanOfRepair.java
@@ -0,0 +1,12 @@
+package com.niklas2233.talismanofrepair;
+
+import net.fabricmc.api.ModInitializer;
+
+public class TalismanOfRepair implements ModInitializer {
+    public static final String MOD_ID = "talismanofrepair";
+
+    @Override
+    public void onInitialize() {
+        // Item registration added in Task 2.
+    }
+}
